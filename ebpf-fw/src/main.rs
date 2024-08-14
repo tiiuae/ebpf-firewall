8de40b9e603ea5b23dceac059bf07a562ab33686
Enes Öztürk <enes.ozturk@unikie.com>
2024-08-15 00:08:55 +0300

    multiple root compilation
    Signed-off-by: Enes Öztürk <enes.ozturk@unikie.com>

--- ebpf-fw/src/main.rs
+++ ebpf-fw/src/main.rs
@@ -19,16 +19,7 @@ async fn main() -> Result<(), anyhow::Error> {
 
     env_logger::init();
 
-    // Bump the memlock rlimit. This is needed for older kernels that don't use the
-    // new memcg based accounting, see https://lwn.net/Articles/837122/
-    let rlim = libc::rlimit {
-        rlim_cur: libc::RLIM_INFINITY,
-        rlim_max: libc::RLIM_INFINITY,
-    };
-    let ret = unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlim) };
-    if ret != 0 {
-        debug!("remove limit on locked memory failed, ret is: {}", ret);
-    }
+
 
     // This will include your eBPF object file as raw bytes at compile-time and load it at
     // runtime. This approach is recommended for most real-world use cases. If you would
@@ -36,11 +27,11 @@ async fn main() -> Result<(), anyhow::Error> {
     // reach for `Bpf::load_file` instead.
     #[cfg(debug_assertions)]
     let mut bpf = Bpf::load(include_bytes_aligned!(
-        "../../target/bpfel-unknown-none/debug/ebpf-fw"
+        "../../target/bpfel-unknown-none/debug/ebpf-fw-ebpf"
     ))?;
     #[cfg(not(debug_assertions))]
     let mut bpf = Bpf::load(include_bytes_aligned!(
-        "../../target/bpfel-unknown-none/debug/ebpf-fw"
+        "../../target/bpfel-unknown-none/release/ebpf-fw-ebpf"
     ))?;
     if let Err(e) = BpfLogger::init(&mut bpf) {
         // This can happen if you remove all log statements from your eBPF program.
